b489de5b4ef02290be29b60bf4b99f69285a46cf
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-07-18 00:14:04 +0700

    changed: out put from number to image dice for real and add contentDescription for blind man

--- app/src/main/java/com/hexademical/diceroller/MainActivity.kt
+++ app/src/main/java/com/hexademical/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hexademical.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +20,23 @@ class MainActivity : AppCompatActivity() {
      *
      *  rollDice function
      *  make a new dice has 6 sides
-     *  and roll number to display on phone screen
+     *  display image dice siede that get from rolled number to shwo on phone screen
      *
      */
     private fun rollDice(){
         val dice = Dice(6) // Create dice
         val rolledNumber = dice.roll() // Roll dice and assign to 'rolledNumber'
-        val rollText: TextView = findViewById(R.id.roll_number) // Get the roll_number text view
-        rollText.text = rolledNumber.toString() // set a rolled number to textview ( don't forget to change it to string)
+        val diceImage: ImageView = findViewById(R.id.roll_image) // Get the roll_number text view
+
+        val diceSideImage = when (rolledNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(diceSideImage)
+        diceImage.contentDescription = "Rolled get number ${rolledNumber.toString()}"
     }
 }
